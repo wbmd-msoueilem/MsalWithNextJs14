9ad09d037ed323c29ce519ebf60e48c55342af6c
Mohamed Soueilem <msoueilem@webmd.net>
2025-05-02 00:33:47 -0400

    config

--- msal/authConfig.js
+++ msal/authConfig.js
@@ -1,14 +1,14 @@
-const CLIENT_ID = "YourClientID";
-export const API_SCOPE = "api://" + CLIENT_ID + "/YourAppName";
+const CLIENT_ID = "f8ec974a-0ad3-4d38-8178-3a6d3bca09bd";
+export const API_SCOPE = "api://" + CLIENT_ID + "/resturantscope";
 
 export const msalConfig = {
     auth: {
         clientId: CLIENT_ID,
-        authority: "https://login.microsoftonline.com/YourTenantID",
+        authority: "https://login.microsoftonline.com/c004a4a0-0496-4499-845f-36c586dc751c",
         redirectUri: "/",
         postLogoutRedirectUri: "/",
         scope: API_SCOPE,
-        domain: "YourDomain",
+        domain: "soueilemlebjawi.onmicrosoft.com",
     },
     cache: {// Optional
         cacheLocation: 'localStorage',  // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
